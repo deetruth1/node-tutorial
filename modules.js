5d53fdd1ab8c4aaec990372150a81b464f700740
deetruth1 <samuelmosesojonugwa@gmail.com>
2024-02-26 13:36:43 -0800

    first commit

---
+++ modules.js
@@ -0,0 +1,29 @@
+// const amount = 12
+
+// if (amount<10){
+//     console.log('small number');
+// }
+// else{
+//     console.log('large number');
+// }
+// console.log('hey its my first node program ');
+//  console.log(__dirname);
+// setInterval(() =>{
+//     console.log('HELLO WORLD');
+// },1000)
+// const names = require('./module1')
+const sayHi = require('./module2')
+const truth = require('./alternative')
+require('./mind-grenade')
+// console.log(names);
+// console.log(sayHi);
+// console.log(truth);
+// const sayHi = (name) =>{
+//     console.log(`hello there ${name}`)
+// }
+
+
+
+// sayHi("moses")
+// sayHi(john)
+// sayHi(peter)
---
+++ path.js
@@ -0,0 +1,14 @@
+const { log } = require('console');
+const path = require('path')
+
+
+console.log(path.sep);
+
+const filepath = path.join('/content', 'subfolder', 'test.txt')
+console.log(filepath);
+
+const basename =  path.basename(filepath)
+console.log(basename);
+
+const absolute = path.resolve(__dirname, 'content', 'subfolder', 'test.txt')
+console.log(absolute);
